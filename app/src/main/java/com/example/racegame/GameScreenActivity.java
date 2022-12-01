c5d01419af551d7c81f9a3928debcac41447e570
Daniel14897 <60986160+Daniel14897@users.noreply.github.com>
2022-12-01 17:33:27 +0200

    logic was changed

--- app/src/main/java/com/example/racegame/GameScreenActivity.java
+++ app/src/main/java/com/example/racegame/GameScreenActivity.java
@@ -5,22 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Looper;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.textview.MaterialTextView;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class GameScreenActivity extends AppCompatActivity {
+    private Vibrator v;
+    private Toast t;
     private ExtendedFloatingActionButton game_FAB_left;
     private ExtendedFloatingActionButton game_FAB_right;
     private MaterialTextView game_LBL_time;
@@ -29,40 +35,41 @@ public class GameScreenActivity extends AppCompatActivity {
     private LinearLayout game_LL_characterLayout;
     private ArrayList<ShapeableImageView> game_character_positions = new ArrayList<ShapeableImageView>();
     private LinearLayout game_LL_obstaclesLayout;
+    private ArrayList<LinearLayout> allRoutesLayouts = new ArrayList<LinearLayout>();
+    private ShapeableImageView obstaclesImages[][];
     private GameManager gm;
+    private int summonObstacle = 1;
     private final int NUMBER_OF_POSITIONS = 3;
     //time variables
     long startTime = 0;
     final int DELAY = 1000;
-    ExecutorService threadPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
         gm = new GameManager();
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         findViews();
         game_FAB_left.setOnClickListener(v -> moveLeft());
         game_FAB_right.setOnClickListener(v -> moveRight());
         initCharacterPositions();
         startTimer();
-        //playBackgroundMusic();
-        threadPool = Executors.newFixedThreadPool(2);
+        playBackgroundMusic();
         startTime = System.currentTimeMillis();
     }
 
-    private void runObstaclesTasks() {
-                threadPool.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        createObstacle();
-                        //delay();
-                    }
-                });
+    @Override
+    protected void onStop() {
+        super.onStop();
+        v.cancel();
+        if(t != null)
+            t.cancel();
     }
 
+
     private void playBackgroundMusic() {
-        MediaPlayer ring= MediaPlayer.create(this,R.raw.crowd);
+        MediaPlayer ring = MediaPlayer.create(this, R.raw.crowd);
         ring.setLooping(true);
         ring.start();
     }
@@ -72,26 +79,28 @@ public class GameScreenActivity extends AppCompatActivity {
     //so i don't want to include them
     private void initCharacterPositions() {
         int size = game_LL_characterLayout.getChildCount();
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             View v = game_LL_characterLayout.getChildAt(i);
-            if(v instanceof ShapeableImageView){
+            if (v instanceof ShapeableImageView) {
                 game_character_positions.add((ShapeableImageView) v);
             }
         }
     }
 
     private Timer timer;
+
     private void startTimer() {
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                runOnUiThread(()->{
+                runOnUiThread(() -> {
                     updateTimerUI();
-                    runObstaclesTasks();
                 });
+                createObstacle();
+                summonObstacle++;
             }
-        },DELAY,DELAY);
+        }, DELAY, DELAY);
     }
 
     private void updateTimerUI() {
@@ -99,17 +108,56 @@ public class GameScreenActivity extends AppCompatActivity {
         int seconds = (int) (millis / 1000);
         int minutes = seconds / 60;
         seconds = seconds % 60;
-        int hours =minutes / 60;
+        int hours = minutes / 60;
         hours %= 24;
-        game_LBL_time.setText(String.format("%02d:%02d:%02d", hours,minutes, seconds));
+        game_LBL_time.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
     }
 
     private void createObstacle() {
-        Obstacle o = new Obstacle();
-        int index = getRandomNum(0,NUMBER_OF_POSITIONS);
-        LinearLayout chosenLayout = (LinearLayout) game_LL_obstaclesLayout.getChildAt(index);
-        int numberOfObstaclesPosition = gm.getNumberOfObstaclesPosition();
-        obstacleMovement(o, index, chosenLayout, numberOfObstaclesPosition);
+        int numberOfObstaclesPosition = gm.getNumberOfObstaclesPosition() - 1;
+        //obstacleMovement(o, index, chosenLayout, numberOfObstaclesPosition);
+        while (numberOfObstaclesPosition > 0) {
+            for (LinearLayout l : allRoutesLayouts) {
+                ShapeableImageView imageBelow = (ShapeableImageView) l.getChildAt(numberOfObstaclesPosition);
+                ShapeableImageView imageAbove = (ShapeableImageView) l.getChildAt(numberOfObstaclesPosition - 1);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageBelow.setVisibility(imageAbove.getVisibility());
+                        imageAbove.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+            if (isCollision()) {
+                setLifeImages();
+                gm.setWrong();
+                String msg = "Ouch! " + String.valueOf(gm.getLifeCount()) + " lives left";
+                toast(msg);
+                gm.vibrate(v);
+            }
+            numberOfObstaclesPosition--;
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                for (LinearLayout l : allRoutesLayouts) {
+                    l.getChildAt(0).setVisibility(View.INVISIBLE);
+                }
+                int index = getRandomNum(0, NUMBER_OF_POSITIONS);
+                allRoutesLayouts.get(index).getChildAt(0).setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private boolean isCollision() {
+        int playerPosition = gm.getPlayer().getCurrentPosition();
+        int bottomPosition = gm.getNumberOfObstaclesPosition()-2;
+        for (int i = 0; i < allRoutesLayouts.size(); i++) {
+            if(allRoutesLayouts.get(i).getChildAt(bottomPosition).getVisibility() == View.VISIBLE && playerPosition == i)
+                    return true;
+        }
+        return false;
     }
 
     //the max num to be generated is "to" minus 1
@@ -117,28 +165,30 @@ public class GameScreenActivity extends AppCompatActivity {
         return from + (int)(Math.random() * (to - from));
     }
 
-    public void obstacleMovement(Obstacle o, int index, LinearLayout chosenLayout, int numberOfObstaclesPosition) {
-        for (int i = 0; i < numberOfObstaclesPosition; i++) {
-            ShapeableImageView obstacleImg = (ShapeableImageView) chosenLayout.getChildAt(o.getPosition());
-            showObstacle(obstacleImg);
-            if(gm.isCollision(index, o.getPosition())){
-                setLifeImages();
-                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                String msg = "Ouch! " + String.valueOf(gm.getLifeCount()) + " lives left";
-                toast(msg);
-                gm.vibrate(v);
-            }
-            delay();
-            hideObstacle(obstacleImg);
-            o.setNextPosition();
-        }
-    }
+//    public void obstacleMovement(Obstacle o, int index, LinearLayout chosenLayout, int numberOfObstaclesPosition) {
+//        for (int i = 0; i < numberOfObstaclesPosition; i++) {
+//            ShapeableImageView obstacleImg = (ShapeableImageView) chosenLayout.getChildAt(o.getPosition());
+//            showObstacle(obstacleImg);
+//            if(gm.isCollision(index, o.getPosition())){
+//                setLifeImages();
+//                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+//                String msg = "Ouch! " + String.valueOf(gm.getLifeCount()) + " lives left";
+//                toast(msg);
+//                gm.vibrate(v);
+//            }
+//            delay();
+//            hideObstacle(obstacleImg);
+//            o.setNextPosition();
+//
+//        }
+//    }
 
     private void toast(String msg) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                gm.toast(msg,GameScreenActivity.this);
+                t = Toast.makeText(GameScreenActivity.this,msg,Toast.LENGTH_SHORT);
+                t.show();
             }
         });
     }
@@ -147,8 +197,10 @@ public class GameScreenActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                int removedIndex = gm.getLifeCount();
-                game_life_images.get(removedIndex).setVisibility(View.INVISIBLE);
+                if(gm.getLifeCount()>=0){
+                    int removedIndex = gm.getLifeCount();
+                    game_life_images.get(removedIndex).setVisibility(View.INVISIBLE);
+                }
             }
         });
 
@@ -171,14 +223,6 @@ public class GameScreenActivity extends AppCompatActivity {
                     obstacleImg.setVisibility(View.VISIBLE);
             }
         });
-    }
-
-    synchronized void delay() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
 
@@ -210,6 +254,14 @@ public class GameScreenActivity extends AppCompatActivity {
         game_LL_obstaclesLayout = findViewById(R.id.game_LL_obstaclesLayout);
         game_LL_heartLayout = findViewById(R.id.game_LL_heartLayout);
         getLifeImages();
+        setAllRoutes();
+    }
+
+
+    private void setAllRoutes() {
+        allRoutesLayouts.add(findViewById(R.id.main_LL_firstLayout));
+        allRoutesLayouts.add(findViewById(R.id.main_LL_secondLayout));
+        allRoutesLayouts.add(findViewById(R.id.main_LL_thirdLayout));
     }
 
     private void getLifeImages() {
